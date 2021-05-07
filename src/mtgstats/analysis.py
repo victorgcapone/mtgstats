52d755629e323896f5705ba92847dbfac2100835
Victor Capone <v.caponemeirelles@gmail.com>
2021-05-07 02:40:53 -0300

    Adicionando parametro de normalização da soma cumulativa à analise de cmc

--- src/mtgstats/analysis.py
+++ src/mtgstats/analysis.py
@@ -3,7 +3,10 @@ from pandas import Series
 """
 Takes a list of cards and compute frequency and cumulative sum arrays for it
 """
-def cmc_analysis(cards):
+def cmc_analysis(cards, normalize_cumsum = False):
     cmcs = map(lambda x:x['cmc'], cards)
     series = Series(cmcs).value_counts().sort_index()
-    return series.to_dict(), series.cumsum().to_dict()
+    cumsum = series.cumsum()
+    if normalize_cumsum:
+        cumsum /= len(cards)
+    return series.to_dict(), cumsum.to_dict()
