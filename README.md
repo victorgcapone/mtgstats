71d2183060f87db03fb481af95822a8621d93563
Victor Capone <v.caponemeirelles@gmail.com>
2021-05-07 21:44:13 -0300

    Adicionando parametro para forçar o download de arquivos de dados dos testes

---
+++ README.md
@@ -0,0 +1,27 @@
+# MTG Stats
+
+
+## Instalando o repositório
+
+Na pasta raíz, executar:
+
+```
+pip install . -r requirements.txt
+```
+
+## Rodando os testes
+
+Na pasta raíz do repositório, executar
+
+```
+./src/tests/run_tests.py
+```
+
+Esse script executa as buscas em src/tests/data/test_queries.txt e gera os arquivos de cartas correspondentes
+na pasta /data, cada linha do aquivo é
+
+```
+<nome_do_arquivo>|<query do scryfall>
+```
+
+Você pode usar o parametro ```--no-cache``` para forçar o Download de arquivos q já existem
--- src/tests/run_tests.sh
+++ src/tests/run_tests.sh
@@ -1,13 +1,18 @@
 #!/bin/zsh
 echo "Running Tests"
-
 # Download the card data used during tests
 echo "Downloading test card data"
 
 for l in `cat src/tests/data/test_queries.txt`; do
 file=`echo $l | cut -d\| -f 1`;
 query=`echo $l | cut -d\| -f 2`;
-python src/scripts/query2file.py --query "$query" --output-file "src/tests/data/$file.json"
+# If the file doesn't exist or if we forced the download
+if [[ ! -f "src/tests/data/$file.json" ]] || [[ $1 = '--no-cache' ]]
+then
+    echo "---> $file data"
+    python src/scripts/query2file.py --query "$query" --output-file "src/tests/data/$file.json"
+fi
 done;
+
 
 python -m unittest -v src/tests/*.py
