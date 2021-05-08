7361b0bb0364c86e3117bdf95e134e871d6533b0
Victor Capone <v.caponemeirelles@gmail.com>
2021-05-07 22:39:15 -0300

    Adicionando notebook de template para analise de statisticas básicas

--- src/mtgstats/plots.py
+++ src/mtgstats/plots.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
-from mtgstats.analysis import cmc_analysis
+from mtgstats.analysis import cmc_analysis, color_analysis
+import pandas as pd
 
 def cmc_distribution(cards, ax=None):
     dist, cumsum = cmc_analysis(cards, normalize_cumsum=True)
 
     if ax == None:
         fig, ax = plt.subplots(figsize = (10,4));
+
+        ax.set(
+            title='CMC Distribution',
+            xlabel = 'CMC',
+            ylabel = 'Freq.',
+            xticks = list(dist.keys())
+        )
     else:
         fig = ax.figure
-
-    ax.set(
-        title='CMC Distribution',
-        xlabel = 'CMC',
-        ylabel = 'Freq.',
-        xticks = list(dist.keys())
-    )
 
     cumsum_ax = ax.twinx()
     cumsum_ax.set(
@@ -23,7 +24,6 @@ def cmc_distribution(cards, ax=None):
 
     ax.bar(dist.keys(), dist.values())
     cumsum_ax.plot(cumsum.keys(), cumsum.values(), c='salmon', label='Cum. Sum.')
-    fig.legend(loc=(.1, .7))
     fig.tight_layout()
     plt.close()
     return fig
@@ -34,15 +34,17 @@ def color_distribution(cards, ax = None):
     colors_freq = colors_freq.sort_values(ascending=False)
     
     if ax == None:
-        fig, ax = plt.subplots( figsize=(10,4))
+        fig, ax = plt.subplots(figsize=(10,4))
+
+                
+        ax.set(
+            title='Amount of cards for each color',
+            xlabel='Color',
+            ylabel='Amount of cards'
+        )
+
     else:
         fig = ax.figure
-        
-    ax.set(
-        title='Amount of creatures for each color',
-        xlabel='Color',
-        ylabel='Amount of creatures'
-    )
 
     # he cumulative sum will exceed the total number of cards if there are multicolored cards
     # as they count for all of their colors
@@ -60,20 +62,21 @@ def not_in_color_distribution(cards, ax = None):
     colors_freq = pd.Series(colors)
     colors_freq = colors_freq.sort_values(ascending=False)
     
+    # The number of cards not of each color is the total minus the amount of creatures in that color
+    not_in_color = (len(cards) - colors_freq).sort_values(ascending=False)
+
     if ax == None:
         fig, ax = plt.subplots(figsize = (10,4))
+
+        ax.set(
+            title = 'Amount of cards NOT in each color',
+            xlabel='Color',
+            ylabel='Amount of cards',
+            ylim=(min(not_in_color) - len(cards)/2,len(cards))
+        )
     else:
         fig = ax.figure
 
-    # The number of cards not of each color is the total minus the amount of creatures in that color
-    not_in_color = (len(data) - colors_freq).sort_values(ascending=False)
-
-    ax.set(
-        title = 'Amount of creatures NOT in each color',
-        xlabel='Color',
-        ylabel='Amount of creatures',
-        ylim=(min(not_in_color) - len(data)/2,len(data))
-    )
 
     ax.bar(not_in_color.index, not_in_color)
     cumsum_ax = ax.twinx()
